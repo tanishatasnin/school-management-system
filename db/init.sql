a77710d3221917f9f8c16fda77ba733d7e24f5f7
tanisha tasnin <tasnintanisha@gmail.com>
2024-01-12 19:57:15 +0600

    add

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS Sstudentdb; 
+CREATE DATABASE IF NOT EXISTS studentdb; 
 USE studentdb; 
 DROP TABLE IS EXISTS students ;
 CREATE TABLE students(
@@ -9,9 +9,20 @@ CREATE TABLE students(
  email  VARCHAR(255) DEFAULT NULL ,
  phone  VARCHAR(255) DEFAULT NULL ,
  address VARCHAR(255) DEFAULT NULL, 
- diagnosis VARCHAR(255) DEFAULT NULL,
  image_url VARCHAR(255) DEFAULT NULL, 
  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
  PRIMARY KEY (id) , 
  CONSTRAINT UQ_PATIENTS_EMAIL UNIQUE (email) 
  );
+
+SHOW TABLE students; 
+INSERT INTO students(first_name, last_name, email, address, phone, image_url)
+ VALUES 
+ (first_name, last_name, email, address, phone, image_url);
+
+
+    SELECT * FROM students; 
+
+
+
+    
