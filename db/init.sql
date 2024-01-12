4b2fea0acb6fa2b53ee20c968cb5b229a6384434
tanisha tasnin <tasnintanisha@gmail.com>
2024-01-12 20:08:48 +0600

    add more

--- db/init.sql
+++ db/init.sql
@@ -25,4 +25,30 @@ INSERT INTO students(first_name, last_name, email, address, phone, image_url)
 
 
 
-    
+  /* for teacher access */ 
+
+CREATE DATABASE IF NOT EXISTS teacherdb; 
+USE teacherdb; 
+
+DROP TABLE IS EXISTS teachers ;
+CREATE TABLE teachers(
+ id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+ first_name VARCHAR(255) DEFAULT NULL , 
+ mid_name VARCHAR(255) DEFAULT NULL ,
+ last_name  VARCHAR(255) DEFAULT NULL ,
+ email  VARCHAR(255) DEFAULT NULL ,
+ phone  VARCHAR(255) DEFAULT NULL ,
+ address VARCHAR(255) DEFAULT NULL, 
+ image_url VARCHAR(255) DEFAULT NULL, 
+ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+ PRIMARY KEY (id) , 
+ CONSTRAINT UQ_PATIENTS_EMAIL UNIQUE (email) 
+ );
+
+SHOW TABLE teachers; 
+INSERT INTO teachers(first_name, last_name, email, address, phone, image_url)
+ VALUES 
+ (first_name, last_name, email, address, phone, image_url);
+
+
+    SELECT * FROM students; 
