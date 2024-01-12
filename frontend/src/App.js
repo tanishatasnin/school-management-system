1af4158e85ed84b45a97a45e87845d34b330f7f1
tanisha tasnin <tasnintanisha@gmail.com>
2024-01-12 17:51:05 +0600

    message

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState();
+  useEffect(() => {
+    fetch("/api/")
+      .then(res => res.json())
+      .then(res => setMessage(res.message))
+      .catch(console.error);
+  }, [setMessage]);
+  return (
+    <div className="App">
+      <h1>School_managment-system</h1>
+    </div>
+  );
+}
+
+export default App;
